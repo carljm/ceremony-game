55bc5bf8cd1e161d74caf921a77b009c8f7f1c5b
Carl Meyer <carl@oddbird.net>
2021-08-17 10:51:15 -0600

    Allow passing in num nodes to render_test.

--- render_test.py
+++ render_test.py
@@ -1,14 +1,16 @@
+import sys
+
 from ceremony.generate import extensions
 from ceremony.geometry import Shape, OR, UR
 from ceremony.render import render_shapes
 
 
-def main() -> None:
+def main(nodes: int) -> None:
     shapes = set()
     shapes.add(Shape.of(OR, UR))
     last = shapes
 
-    for i in range(3):
+    for i in range(nodes - 2):
         new = set()
         for shape in last:
             for ext in extensions(shape):
@@ -16,8 +18,13 @@ def main() -> None:
         shapes.update(new)
         last = new
 
+    print(f"Rendering {len(last)} unique {i+3}-node constellations.")
     render_shapes(last, "example.png")
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        nodes = int(sys.argv[1])
+    else:
+        nodes = 3
+    main(nodes)
